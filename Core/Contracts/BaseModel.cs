24cb9b39c41b2ce9a4de055639bcf3de2cae6173
Yasharvc <yasharvc@yahoo.com>
2020-01-01 16:52:51 +0300

    Adding Contracts Library

---
+++ Core/Contracts/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿ using System;
+
+namespace Contracts
+{
+	public abstract class BaseModel
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ Core/Contracts/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts.Interfaces
+{
+	public interface IRepository<T> where T: BaseModel
+	{
+		void Insert(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+		T GetByID(int id);
+		IEnumerable<T> GetPaged(int countInPage, int pageNumber);
+		IEnumerable<T> GetAll();
+	}
+}
