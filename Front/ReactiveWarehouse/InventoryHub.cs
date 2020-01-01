8b13272862dc33249b7595772c5e65f1e1d71ac1
Yasharvc <yasharvc@yahoo.com>
2020-01-01 17:24:14 +0300

    Initialize web application project

---
+++ Front/ReactiveWarehouse/InventoryHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ReactiveWarehouse
+{
+	public class InventoryHub : Hub
+	{
+		public void Hello()
+		{
+			Clients.All.hello();
+		}
+		public void Send(string name, string message)
+		{
+			// Call the addNewMessageToPage method to update clients.
+			Clients.All.addNewMessageToPage(name, message);
+		}
+	}
+}
---
+++ Front/ReactiveWarehouse/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(ReactiveWarehouse.Startup))]
+namespace ReactiveWarehouse
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// Any connection or hub wire up and configuration should go here
+			app.MapSignalR();
+		}
+	}
+}
