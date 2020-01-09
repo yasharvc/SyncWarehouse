e6a6e04bd5249d50ba1a6b755ec3c357ea4deb7c
Yasharvc <yasharvc@yahoo.com>
2020-01-09 20:26:27 +0300

    Adding Simple Security Policy

---
+++ Front/ReactiveWarehouse/Controllers/SecurityController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactiveWarehouse.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
